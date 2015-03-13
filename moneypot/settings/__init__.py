4e78d23776a71f78288b73a6ae81b872eb447883
Maxim Musayev <shamanu4@gmail.com>
2015-03-13 15:37:57 +0200

    initial django setup

---
+++ moneypot/settings/__init__.py
@@ -0,0 +1,12 @@
+""" Settings for callcenter """
+
+
+class LocalSettingsException(Exception):
+    pass
+
+
+from .base import *
+try:
+    from .local import *
+except ImportError, exc:
+    raise LocalSettingsException('%s (did you rename settings/local-dist.py?)' % exc.args[0])
