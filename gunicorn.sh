e9c11bdb265ce958a82b1e3626e8189063b422ae
Maxim Musayev <shamanu4@gmail.com>
2015-04-29 15:05:14 +0300

    deploy v 0.1.5 gunicorn

---
+++ gunicorn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+LOGFILE=/var/log/gunicorn/moneypot-api.log
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=3
+# user/group to run as
+USER=maxim
+GROUP=maxim
+ADDRESS=127.0.0.1:8002
+cd /home/maxim/production/moneypot-api
+source env/bin/activate
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn ecity.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
+  --user=$USER --group=$GROUP --log-level=debug \
+  --log-file=$LOGFILE 2>>$LOGFILE
