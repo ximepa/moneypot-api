13e58120786aaa1792ad56903f7b82bc1213bb78
Maxim Musayev <shamanu4@gmail.com>
2015-04-30 10:30:32 +0300

    redirect

--- moneypot/urls.py
+++ moneypot/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.authtoken import views as authtoken_views
+from django.views.generic import TemplateView
 
 admin.site.disable_action('delete_selected')
 
@@ -9,8 +10,10 @@ urlpatterns = patterns('',
                        # url(r'^$', 'moneypot.views.home', name='home'),
                        url(r'^base/', include('base.urls', namespace="base")),
                        url(r'^grappelli/', include('grappelli.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
+                       # url(r'^admin/', include(admin.site.urls)),
                        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
                        url(r'^api-token-auth/', authtoken_views.obtain_auth_token),
                        url(r'^autocomplete/', include('autocomplete_light.urls')),
+                       url(r'^$', include(admin.site.urls)),
+                       url(r'^admin/', TemplateView.as_view(template_name="base/redirect.html")),
                        )
