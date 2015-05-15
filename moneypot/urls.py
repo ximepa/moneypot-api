da55aff098bd6193135ecc04b3bb5c700f48e5c8
Maxim Musayev <shamanu4@gmail.com>
2015-05-15 13:09:41 +0300

    temporary serve media files by django when debug is on

--- moneypot/urls.py
+++ moneypot/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns('',
                        url(r'^admin/', TemplateView.as_view(template_name="base/redirect.html")),
                        )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
