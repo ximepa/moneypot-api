bc8cc68b32045a2694429efc1b62ec0ab4c635cd
Maxim Musayev <shamanu4@gmail.com>
2015-05-15 13:09:11 +0300

    temporary serve media files by django

--- moneypot/urls.py
+++ moneypot/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from rest_framework.authtoken import views as authtoken_views
 from django.views.generic import TemplateView
 from filebrowser.sites import site as fb
+from django.conf.urls.static import static
+from django.conf import settings
 
 admin.site.disable_action('delete_selected')
 
@@ -19,3 +21,5 @@ urlpatterns = patterns('',
                        url(r'', include(admin.site.urls)),
                        url(r'^admin/', TemplateView.as_view(template_name="base/redirect.html")),
                        )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
