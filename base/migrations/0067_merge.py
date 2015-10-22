504333d4869e5dcae643f66532d8cf2369acbd7d
Maxim Musayev <shamanu4@gmail.com>
2015-10-22 18:24:28 +0300

    Merge branch 'validators' and merge migrations

---
+++ base/migrations/0067_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0064_geonames_load_fixture'),
+        ('base', '0066_warranty'),
+    ]
+
+    operations = [
+    ]
