750e4128afbbe8ebece231f928a04603d53317fe
Maxim Musayev <shamanu4@gmail.com>
2016-09-06 17:39:28 +0300

    enable api session auth while in development

--- moneypot/settings/base.py
+++ moneypot/settings/base.py
@@ -148,6 +148,7 @@ VERSIONS_BASEDIR = "_versions"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
