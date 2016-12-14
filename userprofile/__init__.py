64eb1884d91bf55963f9fd97320dedc2cfeeb689
Maxim Musayev <shamanu4@gmail.com>
2016-12-14 15:42:12 +0200

    rename profile model

---
+++ userprofile/__init__.py
@@ -0,0 +1,2 @@
+# -*- encoding: utf-8 -*-
+default_app_config = 'userprofile.apps.ProfileConfig'
---
+++ userprofile/apps.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class ProfileConfig(AppConfig):
+    name = 'profile'
+    verbose_name = "Profiles for users"
